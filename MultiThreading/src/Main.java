2367e60f23750c1241f4306a524eeb4b30753fdc
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2024-02-01 18:50:26 -0500

    create thread 2 class

--- MultiThreading/src/Main.java
+++ MultiThreading/src/Main.java
@@ -2,6 +2,9 @@ public class Main{
     public static void main(String args[]) {
         Thread t1 = new Thread1();
         t1.start();
+        Thread2 temp = new Thread2();
+        Thread t2 = new Thread(temp);
+        t2.start();
 
         for(int i =1; i<=5; i++) {
             System.out.println(Thread.currentThread().getName() + ", i = " + i);
