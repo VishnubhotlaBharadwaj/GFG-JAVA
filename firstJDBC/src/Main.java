91fc364305435ec97ff2d4c11976af3227b62482
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2024-02-02 18:29:34 -0500

    commit demo springboot project

--- firstJDBC/src/Main.java
+++ firstJDBC/src/Main.java
@@ -31,7 +31,7 @@ public class Main {
 
             //6. close the connection.
             con.close();
-            
+
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
