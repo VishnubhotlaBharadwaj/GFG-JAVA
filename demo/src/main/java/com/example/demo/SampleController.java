bf49887ae70e65e58b5bf0ca3dea05b1db9f752f
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2024-02-03 16:39:37 -0500

    Update SampleController.java

--- demo/src/main/java/com/example/demo/SampleController.java
+++ demo/src/main/java/com/example/demo/SampleController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,10 @@ public class SampleController {
     //base url - 127.0.0.1:8080
     //api path - /sample, /hello
     //input
-
+    private  static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
     @GetMapping("/hello")
     public String sayHello(@RequestParam("n") String name) {
+        logger.info("Saying hello to {}", name);
         return "hello " + name;
     }
 }
