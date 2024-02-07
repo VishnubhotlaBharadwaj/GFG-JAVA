504f7e21f303a383a975252a55967539aafc3ef5
Bharadwaj Vishnubhotla <vbharadwaj3456@gmail.com>
2024-02-06 21:15:23 -0500

    commit

--- demo-apis/src/main/java/com/example/demoapis/repository/EmployeeRepository.java
+++ demo-apis/src/main/java/com/example/demoapis/repository/EmployeeRepository.java
@@ -1,11 +1,13 @@
 package com.example.demoapis.repository;
 
 import com.example.demoapis.models.Employee;
+import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Repository
 public class EmployeeRepository {
 
     private HashMap<String, Employee> employeeHashMap = new HashMap<>();
--- demo-apis/src/main/java/com/example/demoapis/service/EmployeeService.java
+++ demo-apis/src/main/java/com/example/demoapis/service/EmployeeService.java
@@ -4,9 +4,11 @@ import com.example.demoapis.repository.EmployeeRepository;
 import com.example.demoapis.dto.CreateEmployeeRequest;
 import com.example.demoapis.models.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class EmployeeService {
 
     @Autowired
